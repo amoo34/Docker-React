c56b83299db2cda7f4c931a618ea07a2f3c34580
amoo34 <a.moizfaisal123@gmail.com>
2023-01-31 22:43:06 +0500

    second

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> as if we are 
+          Edit <code>src/App.js</code> as if moi are 
         </p>
         <a
           className="App-link"
